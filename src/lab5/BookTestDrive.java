ac6b193af30f543f18fd162ae04760abc3d3485b
MorganLF <hoangjack514@gmail.com>
2021-10-13 20:26:50 +0700

    updateCode

---
+++ src/lab5/BookTestDrive.java
@@ -0,0 +1,26 @@
+package lab5;
+
+import java.util.Scanner;
+
+public class BookTestDrive {
+    String price ;
+    String publisher;
+    String productyear;
+    String year;
+    String numbers;
+    String type;
+    Scanner scanner = new Scanner(System.in);
+    void getPrice() {
+        System.out.print("giá tiền của sách: ");
+        price = scanner.nextLine();
+        
+    }
+
+    }
+
+
+}
+
+
+     
+   
